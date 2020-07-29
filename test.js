f24519696eeeffe27d799fff5d0e2a7df606956c
Mark <markandrewhamm@gmail.com>
2020-07-28 21:31:23 -0500

    more javascript practice

--- test.js
+++ test.js
@@ -71,14 +71,14 @@ cityPopulations.sf = 800000; // same as cityPopulations["sf"]
 // Write a method that accepts an array of strings and returns a new array that has the string "awesomesauce" inserted between every string.
 // For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
 
-// function insertAwesomesauce(array) {
-//   var newArray = [];
-//   array.forEach(function (string) {
-//     newArray.push(string);
-//     newArray.push("awesomesauce");
-//   });
-//   return newArray;
-// }
+function insertAwesomesauce(array) {
+  var newArray = [];
+  array.forEach(function (string) {
+    newArray.push(string);
+    newArray.push("awesomesauce");
+  });
+  return newArray;
+}
 
 // console.log(insertAwesomesauce(["a", "b", "c", "d", "e"]));
 
@@ -103,14 +103,14 @@ cityPopulations.sf = 800000; // same as cityPopulations["sf"]
 // Write a method that accepts a number and returns its factorial.
 // For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
 
-// function factorial(integer) {
-//   var total = 1;
-//   while (integer > 0) {
-//     total *= integer;
-//     integer--;
-//   }
-//   return total;
-// }
+function factorial(integer) {
+  var total = 1;
+  while (integer > 0) {
+    total *= integer;
+    integer--;
+  }
+  return total;
+}
 
 // console.log(factorial(10));
 
@@ -119,12 +119,209 @@ cityPopulations.sf = 800000; // same as cityPopulations["sf"]
 
 function addCombinations(array1, array2) {
   var newArray = [];
-  array1.forEach(function (array1) {
-    array2.forEach(function (array2) {
-      newArray.push(array1 + array2);
+  array1.forEach(function (numbers1) {
+    array2.forEach(function (numbers2) {
+      newArray.push(numbers1 + numbers2);
     });
   });
   return newArray;
 }
 
-console.log(addCombinations([1, 5, 10], [100, 500, 1000]));
+// console.log(addCombinations([1, 5, 10], [100, 500, 1000]));
+
+function selectEvenItems(strings) {
+  var results = [];
+  for (var i = 0; i < strings.length; i += 2) {
+    results.push(strings[i]);
+  }
+  return results;
+}
+
+// console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]));
+
+function max(numbers) {
+  var maxNumber = numbers[0];
+  var i = 1;
+  while (i < numbers.length) {
+    if (numbers[i] > maxNumber) {
+      maxNumber = numbers[i];
+    }
+    i++;
+  }
+  return maxNumber;
+}
+
+// console.log(max([5, 4, 8, 1, 2]));
+
+function factorials(number) {
+  var total = 1;
+  for (var i = 1; i <= number; i++) {
+    total *= i;
+  }
+  return total;
+}
+
+// console.log(factorials(5));
+
+function descending(numbers) {
+  var descendingNumbers = [];
+  var i = numbers.length - 1;
+  while (i >= 0) {
+    descendingNumbers.push(numbers[i]);
+    i--;
+  }
+  return descendingNumbers;
+}
+
+// console.log(descending([1, 3, 5, 7, 9, 23]));
+
+function sumCombinations(numbers1, numbers2) {
+  var combinations = [];
+  var i = 0;
+  while (i < numbers1.length) {
+    var j = 0;
+    while (j < numbers2.length) {
+      combinations.push(numbers1[i] + numbers2[j]);
+      j++;
+    }
+    i++;
+  }
+  return combinations;
+}
+
+// console.log(sumCombinations([1, 5, 10], [100, 500, 1000]));
+
+function bubbleSort(array) {
+  var n = array.length;
+  var swapped = true;
+  while (swapped === true) {
+    swapped = false;
+    for (var i = 0; i < n - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        array[i], (array[i + 1] = array[i + 1]), array[i];
+        swapped = true;
+      }
+    }
+  }
+  return array;
+}
+
+// console.log(bubbleSort([4, 6, 43, 2, 456, 41]));
+
+// 1. Write a function that takes in an array of numbers and returns its sum.
+
+function sumNumbers(array) {
+  var sum = 0;
+  array.forEach(function (number) {
+    sum += number;
+  });
+  return sum;
+}
+
+// console.log(sumNumbers([4, 5, 10, 1]));
+
+// 2. Write a function that takes in an array of strings and returns the smallest string.
+
+function smallestString(array) {
+  var currentSmallest = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i].length < currentSmallest.length) {
+      currentSmallest = array[i];
+    }
+  }
+  return currentSmallest;
+}
+
+// console.log(smallestString(["this", "is", "a", "string"]));
+
+// 3. Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+
+function reverseOrder(numbers) {
+  var i = numbers.length - 1;
+  var reversed = [];
+  while (i >= 0) {
+    reversed.push(numbers[i]);
+    i--;
+  }
+  return reversed;
+}
+
+// console.log(reverseOrder([4, 6, 8, 10]));
+
+// 4. Write a function that takes in an array of words and returns the number of words that begin with the letter "a".
+
+function aWords(array) {
+  var results = [];
+  array.forEach(function (word) {
+    if (word[0].toLowerCase() === "a") {
+      results.push(word);
+    }
+  });
+  return results;
+}
+
+// console.log(aWords(["Apple", "clap", "applause", "crater"]));
+
+// 5. Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+
+function joinStrings(array) {
+  var result = "";
+  array.forEach(function (word) {
+    result += word + ",";
+  });
+  return result.slice(0, -1);
+}
+
+// console.log(joinStrings(["Apple", "clap", "applause", "crater"]));
+
+// 6. Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+
+function productOfNumbers(array) {
+  var total = 1;
+  for (var i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
+}
+
+// console.log(productOfNumbers([4, 5, 6, 7]));
+
+// 7. Write a function that takes in an array of numbers and returns the two smallest numbers.
+
+function twoSmallestNumbers(array) {
+  var smallest = array[0];
+  var secondSmallest = array[0];
+  array.forEach(function (number) {
+    if (number < smallest) {
+      // console.log("hello");
+      secondSmallest = smallest;
+      smallest = number;
+    } else if (number < secondSmallest) {
+      secondSmallest = number;
+    }
+  });
+  return [smallest, secondSmallest];
+}
+
+// console.log(twoSmallestNumbers([67, 4, 899, 2, 66]));
+
+// 8. Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+
+function howManyZeros(array) {
+  var numberOfZeros = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 0) {
+      numberOfZeros++;
+    }
+  }
+  return numberOfZeros;
+}
+
+console.log(howManyZeros([4, 6, 0, 2, 1, 0]));
+
+// 9. Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+// 10. Write a function that takes in an array of words and returns the number of times the letter "a" appeared in total.
+// BONUS PROBLEMS
+// Write a function that accepts a string and returns whether it's a palindrome.
+// Write a function to generate/print/store the first "n" prime numbers.
+// Given a tic-tac-toe board (matrix of 3 x 3), write a function that can check to see whether X or O won.
